35e701052960b31ec9f095266ec3f2202650ac9d
HAILGAMBO <gauravk.scb@gmail.com>
2018-03-15 04:04:21 -0700

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+# Twitter API Keys
+consumer_key = "AJZgpyXOGHz157vwdwLtlAgfH"
+consumer_secret = "SsWxLvhkz7bshTd9k97OGRSXCuomsvJoNYJVkwL94Yvl85487a"
+access_token = "388432996-D4GCl1UpUPkOwGbrdgQ1WNIFNQGRYJJnMPMOr5jx"
+access_token_secret = "al5ANSITQ9AdBkU2Yvi3KsX4VTmXIY6G5qg9U7wjDuZ8H"
+openweather_api_key = "a14042ede3cc43ca15957fab64575481"
